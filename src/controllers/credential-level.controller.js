35a6042b00b8ef61ebde5ffdc35e4e632ba03bdd
programmerkenzi <a23g32@gmail.com>
2023-12-06 11:43:26 +0800

    IVERN-390 refactor: get credential levels api

--- src/controllers/credential-level.controller.js
+++ src/controllers/credential-level.controller.js
@@ -1,30 +1,9 @@
 import { CredentialLevel } from '../models';
-import stub from '../models/credential-levels/credential-levels.stub';
-import {
-  assertHomeOrgExists,
-  assertIfReadOnlyMode,
-  assertNoPendingCommits,
-} from '../utils/data-assertions';
 
 export const findAll = async (req, res) => {
   try {
     // create new table if it doesn't exist
     let credentialLevels = await CredentialLevel.findAll();
-    await assertIfReadOnlyMode();
-    await assertNoPendingCommits();
-    await assertHomeOrgExists();
-
-    // if no credential levels are found, insert default stub
-    if (credentialLevels.length === 0) {
-      // create new credential level table if it doesn't exist
-      credentialLevels = [];
-      for (const level of stub) {
-        const newLevel = await CredentialLevel.create(level);
-        credentialLevels.push(newLevel);
-      }
-
-      res.status(200).json(credentialLevels);
-    }
 
     res.status(200).json(credentialLevels);
   } catch (error) {
--- src/routes/v1/resources/credential-level.js
+++ src/routes/v1/resources/credential-level.js
@@ -1,12 +1,9 @@
 'use strict';
 
 import express from 'express';
-import joiExpress from 'express-joi-validation';
 
 import { credentialLevelController } from '../../../controllers';
-import { credentialLevelSchema } from '../../../validations/credential-level-validations';
 
-const validator = joiExpress.createValidator({ passError: true });
 const CredentialLevelRouter = express.Router();
 
 CredentialLevelRouter.get('/', (req, res) => {
